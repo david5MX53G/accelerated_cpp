53ae9558c1b3467a465f5ce3263e3a3900b6d0f1
Branch <branch@localhost.localdomain>
2019-06-15 09:03:19 -0500

    added fancy framed greeting

--- main.cpp
+++ main.cpp
@@ -3,9 +3,24 @@
 
 int main()
 {
-	std::cout << "First name: ";
+	std::cout << "Please enter your first name: ";
 	std::string name;
 	std::cin >> name;
-	std::cout << "Hello, " << name << "!" << std::endl;
-	return 0;
+
+	// build some constants
+	const std::string greeting = "Hello, " + name + "!";
+	const std::string spaces(greeting.size(), ' ');
+	const std::string second = "* " + spaces + " *";
+	const std::string first(second.size(), '*');
+
+	// write stuff
+	std::cout << std::endl;
+	std::cout << first << std::endl;
+	std::cout << second << std::endl;
+	std::cout << "* " << greeting << " *" << std::endl;
+    std::cout << second << std::endl;
+    std::cout << first << std::endl;
+
+    // return
+    return 0;
 }
