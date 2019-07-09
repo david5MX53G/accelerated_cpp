6896738ca24565898abd53c3335a5dae54ac20d7
Branch <branch@localhost.localdomain>
2019-07-09 07:04:22 -0500

    updated main.cpp from chapter 7

--- main.cpp
+++ main.cpp
@@ -1,21 +1,50 @@
-#include "split.h"
-#include <vector>
 #include <string>
+#include <map>
 #include <iostream>
+#include <vector>
+#include "split.h"
 
 using std::cin;
-using std::cout;
 using std::string;
-using std::vector;
+using std::map;
+using std::cout;
 using std::endl;
+using std::vector;
+using std::istream;
+
+// find all lines referring to each word from input
+map<string, vector<int> > xref(
+        istream& in,
+        vector<string> find_words(const string&) = split // this parameter uses a "default argument"
+    ) {
+    string line;
+    int line_number = 0;
+    map<string, vector<int> > ret;
+
+    while (getline(in, line)) { // getline() is from the string library
+        ++line_number;
+        vector<string> words = find_words(line); // function parameter "find_words"
+        for (vector<string>::const_iterator it = words.begin(); it != words.end(); ++it)
+            ret[*it].push_back(line_number); // append line_number to value of ret[*it] where *it is key of "ret" map
+    }
+    return ret;
+}
 
 int main() {
-    string s;
-    while (getline(cin, s)) {
-        vector<string> v = split(s);
+    map<string, vector<int> > ret = xref(cin);
 
-        for (vector<string>::size_type i = 0; i != v.size(); ++i)
-            cout << v[i] << endl;
+    for (map<string, vector<int> >::const_iterator it = ret.begin(); it != ret.end(); ++it) {
+        cout << it->first << " occurs on line(s): ";
+        vector<int>::const_iterator line_it = it->second.begin();
+        cout << *line_it;
+        ++line_it;
+
+        while (line_it != it->second.end()) {
+            cout << ", " << *line_it;
+            ++line_it;
+        }
+
+        cout << endl;
     }
     return 0;
 }
