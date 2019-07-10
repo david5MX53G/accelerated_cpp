c968ff2c4dd7dd36baa88567892b4397db384233
Branch <branch@localhost.localdomain>
2019-07-10 13:34:12 -0500

    updated and tested code from ch 7

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include "split.h"
+#include <random>
 
 using std::cin;
 using std::string;
@@ -11,40 +12,98 @@ using std::cout;
 using std::endl;
 using std::vector;
 using std::istream;
+using std::domain_error;
+using std::logic_error;
 
-// find all lines referring to each word from input
-map<string, vector<int> > xref(
-        istream& in,
-        vector<string> find_words(const string&) = split // this parameter uses a "default argument"
-    ) {
-    string line;
-    int line_number = 0;
-    map<string, vector<int> > ret;
+typedef vector<string> Rule;
+typedef vector<Rule> Rule_collection;
+typedef map<string, Rule_collection> Grammar;
 
-    while (getline(in, line)) { // getline() is from the string library
-        ++line_number;
-        vector<string> words = find_words(line); // function parameter "find_words"
-        for (vector<string>::const_iterator it = words.begin(); it != words.end(); ++it)
-            ret[*it].push_back(line_number); // append line_number to value of ret[*it] where *it is key of "ret" map
+// istream should be in the format <category> word1 word2 word3 etc.
+Grammar read_grammar(istream& in) {
+   Grammar ret;
+   string line;
+
+   while (getline(in, line)) {
+       vector<string> entry = split(line);
+
+       if(!entry.empty()) {
+           ret[entry[0]].push_back(
+               Rule(entry.begin() + 1, entry.end())
+           );
+       }
+   }
+
+   return ret;
+}
+
+// is the string surrounded by brackets?
+bool bracketed(const string& s) {
+    return s.size() > 1 && s[0] == '<' && s[s.size() - 1] == '>';
+}
+
+// return random number less than 'n'
+int nrand(int n) {
+   if (n <= 0 || n > RAND_MAX)
+       throw domain_error("Arg to nrand is out of range");
+
+   const int bucket_size = RAND_MAX / n;
+   int r;
+
+   do r = rand() / bucket_size;
+   while (r >= n);
+
+   cout << "nrand: " << r << endl;
+   cout << "rand: " << rand() << endl;
+   cout << "bucket_size: " << bucket_size << endl;
+
+   return r;
+}
+
+// expand the second arg using the grammar of the first arg, and store results in third arg
+void gen_aux(const Grammar& g, const string& word, vector<string>& ret) {
+    if (!bracketed(word)) {
+        ret.push_back(word);
+    } else {
+        // locate corresponding rule using map.find()
+        // using g[word] would insert a new value when no match is found
+        Grammar::const_iterator it = g.find(word);
+
+        // map.find() returns the end of the map when no matches are found
+        if (it == g.end())
+            throw logic_error("empty rule");
+
+        // dereferencing a map variable yeilds a "pair" value which always has "first" and "second" members
+        const Rule_collection& c = it->second;
+
+        // select random element of vector from the second value in the Rule_collection pair
+        const Rule& r = c[nrand(c.size())];
+
+        // the key to understanding recursion is to understand recursion
+        for (Rule::const_iterator i = r.begin(); i != r.end(); ++i)
+            gen_aux(g, *i, ret);
     }
+}
+
+vector<string> gen_sentence(const Grammar& g) {
+    vector<string> ret;
+    gen_aux(g, "<sentence>", ret);
     return ret;
 }
 
 int main() {
-    map<string, vector<int> > ret = xref(cin);
-
-    for (map<string, vector<int> >::const_iterator it = ret.begin(); it != ret.end(); ++it) {
-        cout << it->first << " occurs on line(s): ";
-        vector<int>::const_iterator line_it = it->second.begin();
-        cout << *line_it;
-        ++line_it;
-
-        while (line_it != it->second.end()) {
-            cout << ", " << *line_it;
-            ++line_it;
-        }
-
-        cout << endl;
+    vector<string> sentence = gen_sentence(read_grammar(cin));
+    vector<string>::const_iterator it = sentence.begin();
+    if (!sentence.empty()) {
+        cout << *it;
+        ++it;
     }
+
+    while (it != sentence.end()) {
+        cout << " " << *it;
+        ++it;
+    }
+
+    cout << endl;
     return 0;
 }
