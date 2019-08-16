c05203ada965c79767f13b02b2ee1cefbff01695
Branch <branch@localhost.localdomain>
2019-08-16 06:59:39 -0500

    added str.cpp and str.h from chapter 12

---
+++ str.cpp
@@ -0,0 +1,40 @@
+//
+// Created by branch on 7/25/19.
+//
+
+#include "str.h"
+#include "vec.h"
+#include <iostream>
+
+using std::ostream;
+using std::istream;
+
+ostream& operator<<(ostream& os, const Str& s) {
+    for (Str::size_type i = 0; i != s.size(); ++i)
+        os << s[i];
+    return os;
+}
+
+// this won't compile yet
+// pg 216
+istream& operator>>(istream& is, Str& s) {
+    // clear existing value(s)
+    s.data.clear(); // compile err: data is private
+
+    // read and discard leading whites
+    char c;
+    while (is.get(c) && isspace(c)) {
+        ; // nothing to do, except testing the cond
+    }
+
+    if (is) {
+        do s.data.push_back(c); // compile err: data is private
+        while (is.get(c) && !isspace(c));
+
+        // if we read whites, then put it back on the stream
+        if (9s)
+            is.unget();
+    }
+
+    return is;
+}
